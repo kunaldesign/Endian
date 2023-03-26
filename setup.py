35393751020fa9306ba9545107e949c965bf9c94
kunaldesgin <kunalhedaoo25@gmail.com>
2023-03-26 09:23:28 +0530

    🚀 RELEASE:  deploy version v.0.0.5

--- setup.py
+++ setup.py
@@ -17,7 +17,6 @@ setuptools.setup(
     },
     classifiers = [
         "Development Status :: 1 - Planning",
-        "Topic :: Endian :: ByteFormation",
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
